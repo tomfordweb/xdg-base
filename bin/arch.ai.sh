24d0d3addccfa1685ad561da598e1137f495d448
Tom Ford <tomfordweb@gmail.com>
2025-11-17 16:56:58 -0500

    i bricked it lmao

--- bin/arch.ai.sh
+++ bin/arch.ai.sh
@@ -2,7 +2,10 @@
 
 # scripting to get AI up and running in docker containers on Arch Linux
 #
-# Graphics Card: 980GTX 
-#
+# Graphics Card: 
+#   - 980GTX 
+#   - NV124 (NV110 Family (Maxwell))
+# First , install nvidia as it is recommended by arch wiki. I am also on the linux kernel and not linux-lts in which case you would want nvidia-lts
+sudo pacman --needed -Sy nvidia
 sudo pacman --needed -Sy libnvidia-container nvidia-utils opencl-nvidia nvidia-container-toolkit
 
